12568ead9e0858bec372b10f0bd87b72dacbdb4c
Chien <chientd99@gmail.com>
2025-12-14 16:45:46 +0700

    Update: Frontend No 2

--- src/mock/mock.jsx
+++ src/mock/mock.jsx
@@ -1,20 +1,38 @@
-const mock = [
+export const mock = [
 	{
 		id: 1,
 		name: 'Task test',
 		isDone: false,
-		isAbandoned: false,
+		isActive: false,
 	},
 	{
 		id: 2,
 		name: 'Task test',
 		isDone: false,
-		isAbandoned: false,
+		isActive: false,
 	},
 	{
 		id: 3,
 		name: 'Task test',
+		isDone: true,
+		isActive: false,
+	},
+	{
+		id: 4,
+		name: 'Task test',
+		isDone: true,
+		isActive: false,
+	},
+	{
+		id: 5,
+		name: 'Task test',
 		isDone: false,
-		isAbandoned: false,
+		isActive: true,
+	},
+	{
+		id: 6,
+		name: 'Task test',
+		isDone: false,
+		isActive: true,
 	},
 ];
--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -1,5 +1,20 @@
 import css from './Page.module.css';
+import { useContext, useMemo } from 'react';
+import { TodoContext } from '../App';
+import Container from '../components/container/Container';
+import TodoList from '../components/todo-list/TodoList';
 
 export default function Completed() {
-	return <div>Completed</div>;
+	const { list } = useContext(TodoContext);
+
+	const displayList = useMemo(() => {
+		return list.filter((item) => !!item.isDone);
+	}, [list]);
+
+	return (
+		<Container>
+			Completed
+			<TodoList list={displayList} />
+		</Container>
+	);
 }
