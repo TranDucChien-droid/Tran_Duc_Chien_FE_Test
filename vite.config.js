fa35348f22ca0d45a94dad8077314f03912ccf23
Chien <chientd99@gmail.com>
2026-01-07 22:34:55 +0700

    Update yml

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Tran_Duc_Chien_FE_Test",
+  base: "/",
   plugins: [react(), tailwindcss()],
   server: {
     port: 5176
